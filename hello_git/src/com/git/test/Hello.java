2fd05d628cfe55cac8ae17c5a7f9ce429d60cff4
HuangTengyu <1445763458@qq.com>
2016-03-15 14:57:11 +0800

    Update Hello.java
    add a sentense from github

--- hello_git/src/com/git/test/Hello.java
+++ hello_git/src/com/git/test/Hello.java
@@ -3,5 +3,6 @@ package com.git.test;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("wo shi fu wu qi xin tian jia de nei rong");
 	}
 }
